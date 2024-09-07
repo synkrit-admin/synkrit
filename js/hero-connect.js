5f8c666c8d2aaccaba38d07e5fd5cbcca817c971
Jak-J <Jak.t161@gmail.com>
2024-09-07 13:21:15 +0100

    updated hero, added our team

---
+++ js/hero-connect.js
@@ -0,0 +1,7 @@
+function handleConnectClick() {
+    // Example action on button click
+    alert('Thank you for connecting with us! We will reach out to you shortly.');
+    
+    // Scroll smoothly to the "connect" section (if desired)
+    document.getElementById('connect').scrollIntoView({ behavior: 'smooth' });
+  }
